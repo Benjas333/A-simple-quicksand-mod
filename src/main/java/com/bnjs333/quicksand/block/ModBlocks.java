b7c5af89d2b94977c67fecd4f5bac0aeca9938c6
Benjas333 <seston30@gmail.com>
2024-09-09 07:55:32 -0600

    Init commit

--- src/main/java/com/bnjs333/quicksand/block/ModBlocks.java
+++ src/main/java/com/bnjs333/quicksand/block/ModBlocks.java
@@ -1,12 +1,16 @@
 package com.bnjs333.quicksand.block;
 
 import com.bnjs333.quicksand.Quicksand;
+import com.bnjs333.quicksand.block.custom.QuicksandBlock;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
@@ -14,7 +18,7 @@ import net.minecraft.util.Identifier;
 
 public class ModBlocks {
     public static final Block QUICKSAND = registerBlock("quicksand",
-            new QuicksandBlock(FabricBlockSettings.copyOf(Blocks.POWDER_SNOW).sounds(BlockSoundGroup.MUD)));
+            new QuicksandBlock(FabricBlockSettings.copyOf(Blocks.POWDER_SNOW).sounds(BlockSoundGroup.MUD).strength(0.5F)));
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
@@ -26,7 +30,13 @@ public class ModBlocks {
                 new BlockItem(block, new FabricItemSettings()));
     }
 
+    private static void addItemsToNaturalItemGroup(FabricItemGroupEntries entries) {
+        entries.add(QUICKSAND);
+    }
+
     public static void registerModBlocks() {
         Quicksand.LOGGER.info("Registering ModBlocks for " + Quicksand.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register(ModBlocks::addItemsToNaturalItemGroup);
     }
 }
