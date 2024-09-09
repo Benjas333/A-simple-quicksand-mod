797d55a9503074d1a53b4a1ad0788085fbf2dd3f
Benjas333 <seston30@gmail.com>
2024-09-09 08:32:57 -0600

    Added Models and Blocks data generators

--- src/main/java/com/bnjs333/quicksand/QuicksandDataGenerator.java
+++ src/main/java/com/bnjs333/quicksand/QuicksandDataGenerator.java
@@ -1,6 +1,8 @@
 package com.bnjs333.quicksand;
 
+import com.bnjs333.quicksand.datagen.ModBlockTagProvider;
 import com.bnjs333.quicksand.datagen.ModLootTableProvider;
+import com.bnjs333.quicksand.datagen.ModModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
@@ -9,6 +11,8 @@ public class QuicksandDataGenerator implements DataGeneratorEntrypoint {
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModBlockTagProvider::new);
 		pack.addProvider(ModLootTableProvider::new);
+		pack.addProvider(ModModelProvider::new);
 	}
 }
