5b316842abfd6f96f425f120e0d0f3e2319cfd76
Benjas333 <seston30@gmail.com>
2024-09-09 07:55:21 -0600

    Init commit

---
+++ src/main/java/com/bnjs333/quicksand/datagen/ModLootTableProvider.java
@@ -0,0 +1,16 @@
+package com.bnjs333.quicksand.datagen;
+
+import com.bnjs333.quicksand.block.ModBlocks;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
+
+public class ModLootTableProvider extends FabricBlockLootTableProvider {
+    public ModLootTableProvider(FabricDataOutput dataOutput) {
+        super(dataOutput);
+    }
+
+    @Override
+    public void generate() {
+        addDrop(ModBlocks.QUICKSAND);
+    }
+}
