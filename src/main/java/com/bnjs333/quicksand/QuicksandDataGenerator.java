45d50815b779ad8ca51f46c341cf3508f3188cc5
Benjas333 <seston30@gmail.com>
2024-09-09 08:00:39 -0600

    Added custom data generator

--- src/main/java/com/bnjs333/quicksand/QuicksandDataGenerator.java
+++ src/main/java/com/bnjs333/quicksand/QuicksandDataGenerator.java
@@ -1,11 +1,14 @@
 package com.bnjs333.quicksand;
 
+import com.bnjs333.quicksand.datagen.ModLootTableProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class QuicksandDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModLootTableProvider::new);
 	}
 }
